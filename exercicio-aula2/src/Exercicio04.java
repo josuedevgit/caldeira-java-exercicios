93d6818eb375d8708c5e250b1927e5d17c3943d5
Josué Silva <josueluzsilva.01@gmail.com>
2023-09-14 11:20:11 -0300

    correcao no ex 04

--- exercicio-aula2/src/Exercicio04.java
+++ exercicio-aula2/src/Exercicio04.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Exercicio04 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for(String loop = "s"; loop.equals("s"); loop = sc.nextLine()) {
+        String loop = "s";
+        while(loop.equals("s")) {
             int numero1, numero2;
 
+            Scanner sc = new Scanner(System.in);
             System.out.println("Vamos calcular!");
             System.out.println("Escolha uma operação para aplicar [*, /, +, -]:");
             String operacao = sc.nextLine();
@@ -16,6 +17,7 @@ public class Exercicio04 {
 
             System.out.println("Digite outro numero:");
             numero2 = sc.nextInt();
+            sc.nextLine();
 
             switch(operacao) {
                 case "*":
@@ -39,46 +41,7 @@ public class Exercicio04 {
                     break;
             }
             System.out.println("Você deseja calcular novamente:");
-
+            loop = sc.nextLine();
         }
-//        String loop = "s";
-//        while(loop == "s") {
-//            int numero1, numero2;
-//
-//            Scanner sc = new Scanner(System.in);
-//            System.out.println("Vamos calcular!");
-//            System.out.println("Escolha uma operação para aplicar [*, /, +, -]:");
-//            String operacao = sc.nextLine();
-//
-//            System.out.println("Digite o primeiro numero:");
-//            numero1 = sc.nextInt();
-//
-//            System.out.println("Digite outro numero:");
-//            numero2 = sc.nextInt();
-//
-//            switch(operacao) {
-//                case "*":
-//                    int multiplicacao = numero1 * numero2;
-//                    System.out.println(numero1 + " * " + numero2 + " = " + multiplicacao);
-//                    break;
-//                case "/":
-//                    int divisao = numero1 / numero2;
-//                    System.out.println(numero1 + " / " + numero2 + " = " + divisao);
-//                    break;
-//                case "+":
-//                    int adicao = numero1 + numero2;
-//                    System.out.println(numero1 + " + " + numero2 + " = " + adicao);
-//                    break;
-//                case "-":
-//                    int subtracao = numero1 - numero2;
-//                    System.out.println(numero1 + " - " + numero2 + " = " + subtracao);
-//                    break;
-//                default:
-//                    System.out.println("Dado inválido! Digite novamente! [*, /, +, -]");
-//                    break;
-//            }
-//            System.out.println("Você deseja calcular novamente:");
-//            loop = sc.nextLine();
-//        }
     }
 }
