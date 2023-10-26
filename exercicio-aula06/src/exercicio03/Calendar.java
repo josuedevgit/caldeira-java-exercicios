9c238f95cbc1b63f730f58542c9f77756f60e001
Josué Silva <josueluzsilva.01@gmail.com>
2023-10-26 16:21:33 -0300

    criacao de classe Calendar

---
+++ exercicio-aula06/src/exercicio03/Calendar.java
@@ -0,0 +1,10 @@
+package exercicio03;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Calendar {
+    private Map<String, String> nameAndPhoneNumber = new TreeMap<>();
+    private String adress;
+    private String cityLocal;
+}
---
+++ exercicio-aula06/src/exercicio03/Exercicio03.java
@@ -0,0 +1,8 @@
+package exercicio03;
+
+public class Exercicio03 {
+
+    public static void main(String[] args) {
+
+    }
+}
