533b1ee4eb68174078ded242f3470db2e2732812
Josué Silva <josueluzsilva.01@gmail.com>
2023-10-27 09:59:04 -0300

    inicio de metodo para apresentar lista de contatos

--- exercicio-aula06/src/exercicio03/Calendar.java
+++ exercicio-aula06/src/exercicio03/Calendar.java
@@ -1,10 +1,33 @@
 package exercicio03;
 
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Calendar {
-    private Map<String, String> nameAndPhoneNumber = new TreeMap<>();
-    private String adress;
+    private Map<String, String> phoneList = new TreeMap<>();
+    private String address;
     private String cityLocal;
+
+    public Calendar(Map<String, String> phoneList, String address, String cityLocal) {
+        this.phoneList = phoneList;
+        this.address = address;
+        this.cityLocal = cityLocal;
+    }
+
+    public void getPhoneList() {
+        // extrai as chaves que sao nomes
+        Set<String> names = phoneList.keySet();
+
+        // orderna os nomes em ordem alfabetica
+        List<String> ordernedNames = new ArrayList<>(names);
+        Collections.sort(ordernedNames);
+
+//        for (Map.Entry<String, String> contact : phoneList.entrySet()) {
+//            String name = contact.getKey();
+//            String phoneNumber = contact.getValue();
+//
+//            System.out.println("Lista de contatos:");
+//            System.out.println(name + ": " + phoneNumber);
+//            System.out.println("----------------------");
+//        }
+    }
 }
--- exercicio-aula06/src/exercicio03/Exercicio03.java
+++ exercicio-aula06/src/exercicio03/Exercicio03.java
@@ -1,8 +1,41 @@
 package exercicio03;
 
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Exercicio03 {
 
     public static void main(String[] args) {
+        Map<String, String> phoneList = new TreeMap<>();
+        Scanner scanner = new Scanner(System.in);
 
+        // fazer um programa "rotativo"
+//        while(true) {
+//            switch () {
+//
+//            }
+//        }
+
+        System.out.println("LISTA TELEFONICA");
+
+        System.out.print("Digite seu nome: ");
+        String name = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Digite seu número de celular: ");
+        String phoneNumber = scanner.nextLine();
+
+        phoneList.put(name, phoneNumber);
+        System.out.println("--------------------");
+
+        System.out.print("Digite seu endereço: ");
+        String address = scanner.nextLine();
+        System.out.println();
+
+        System.out.print("Digite sua região: ");
+        String localCity = scanner.nextLine();
+
+        Calendar calendar = new Calendar(phoneList, address, localCity);
     }
 }
